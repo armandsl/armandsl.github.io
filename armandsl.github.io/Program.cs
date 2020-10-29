2b1786175676c6d29dc1a8d293dc7f9d29670417
M. Arman Uz Zuha <38503380+armandsl@users.noreply.github.com>
2020-10-29 06:16:56 +0600

    Create package

---
+++ armandsl.github.io/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace armandsl.github.io
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
